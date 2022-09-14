be0bb0d0d9dee3372a0bfae635c22a0a3315f246
jona-11 <jonathangalimberti@hotmail.com>
2022-09-13 22:15:04 -0300

    se agrega llamador para esperar carga del html antes del script

--- mokemon/src/index.js
+++ mokemon/src/index.js
@@ -1,8 +1,12 @@
-document.onload
+function iniciarJuego(){
 
-let botonMascotaJugador= document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click",seleccionMascotaJugador())
+    let botonMascotaJugador= document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click",seleccionMascotaJugador)
+}
+
 
 function seleccionMascotaJugador(){
     alert("seleccionaste a tu mascota")
 }
+
+window.addEventListener("load",iniciarJuego)
