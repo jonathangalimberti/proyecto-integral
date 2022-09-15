a3d9898f6ce1a71a644c4f450faef685803c8c7f
jona-11 <jonathangalimberti@hotmail.com>
2022-09-15 11:40:50 -0300

    estructurado de logica para seleccion de mascota

--- mokemon/src/index.js
+++ mokemon/src/index.js
@@ -4,9 +4,28 @@ function iniciarJuego(){
     botonMascotaJugador.addEventListener("click",seleccionMascotaJugador)
 }
 
-
 function seleccionMascotaJugador(){
-    alert("seleccionaste a tu mascota")
+    let hipodoge= document.getElementById("hipodoge").checked
+    let capipepo= document.getElementById("capipepo").checked
+    let ratigueya= document.getElementById("ratigueya").checked
+    let langostelvis= document.getElementById("langostelvis").checked
+    let tucapalma= document.getElementById("tucapalma").checked
+    let pydos= document.getElementById("pydos").checked
+    
+    if(hipodoge){
+        return alert("seleccionaste a Hipodoge")
+    }else if (capipepo){
+        return alert("seleccionaste a capipepo")
+    }else if (ratigueya){
+        return alert("seleccionaste a ratigueya")
+    }else if (langostelvis){
+        return alert("seleccionaste a langostelvis")
+    }else if (tucapalma){
+        return alert("seleccionaste a tucapalma")
+    }else if (pydos){
+        return alert("seleccionaste a pyros")
+    }
+    
 }
 
 window.addEventListener("load",iniciarJuego)
